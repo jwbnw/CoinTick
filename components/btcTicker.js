5065ee06c3ce3b87e326207bea715e873c7d9f24
jwbnw <james.w.belford@gmail.com>
2018-11-22 10:54:31 -0800

    basic fetch for btc implemented

--- components/btcTicker.js
+++ components/btcTicker.js
@@ -1,51 +1,66 @@
-import React, { Component } from 'react';
-import { Alert, AppRegistry, StyleSheet, View, TouchableOpacity, Text } from 'react-native';
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  Text
+} from "react-native";
 
-
-  export default class BtcTicker extends React.Component {
-
-	constructor(props){
+export default class BtcTicker extends React.Component {
+  constructor(props) {
     super(props);
-    this.state = { isLoading: true}
-  }
+    this.state = { isLoading: true, dataSource: "someVal" };
 
+    //Not sure if there is a better way to do this.
+    this._onPressBtn = this._onPressBtn.bind(this);
+    this.fetchData = this.fetchData.bind(this);
+  }
 
   _onPressBtn() {
-    Alert.alert('BTC!')
+    this.fetchData();
   }
 
+  fetchData() {
+    fetch("https://api.coindesk.com/v1/bpi/currentprice/USD.json")
+      .then(response => response.json())
+      .then(responseJSON => {
+        this.setState({
+          dataSource: responseJSON.bpi.USD.rate,
+          isLoading: false
+        });
+        console.log(this.state.dataSource);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
 
   render() {
-    return (
-      <View>
- 
-          <TouchableOpacity 
-          onPress={this._onPressBtn}
-          style={styles.button1}
-          >  
-              <Text>BTC</Text>
-          
-          </TouchableOpacity>
-      </View>
-
-
-    );
+   
+    if (this.state.isLoading) {
+      return (
+        <TouchableOpacity onPress={this._onPressBtn} style={styles.button1}>
+          <Text>BTC</Text>
+        </TouchableOpacity>
+      );
+    } else {
+      return <Text style={styles.result}>{this.state.dataSource}</Text>;
+    }
   }
-
 }
-
-
 
 const styles = StyleSheet.create({
   button1: {
-    height: 50,   
-    alignItems: 'center',
-    color: 'blue'
+    height: 50,
+    alignSelf: "center",
+    color: "blue"
+  },
+  result: {
+    alignSelf: "center",
+    height: 50
   },
   buttonText: {
     padding: 20,
-    color: 'white'
+    color: "white"
   }
 });
-
-
