e187997f63cc414f513c432351bdb02175c02901
jwbnw <james.w.belford@gmail.com>
2018-11-22 19:47:41 -0800

    updated ltc fetch

--- components/ltcTicker.js
+++ components/ltcTicker.js
@@ -1,48 +1,62 @@
-import React, { Component } from 'react';
-import { Alert, AppRegistry, StyleSheet, View, TouchableOpacity, Text } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, View, TouchableOpacity, Text } from "react-native";
 
-  export default class LtcTicker extends React.Component {
+//import fader from './components/fader.js';
 
-	constructor(props){
+export default class BtcTicker extends React.Component {
+  constructor(props) {
     super(props);
-    this.state = { isLoading: true}
+    this.state = { isLoading: true, dataSource: "" };
+
+    //Not sure if there is a better way to do this.
+    this._onPressBtn = this._onPressBtn.bind(this);
+    this.fetchData = this.fetchData.bind(this);
   }
 
   _onPressBtn() {
-    Alert.alert('LTC!')
+    this.fetchData();
+  }
+
+  fetchData() {
+    fetch("https://api.coinmarketcap.com/v1/ticker/litecoin/")
+      .then(response => response.json())
+      .then(responseJSON => {
+        this.setState({
+          dataSource: responseJSON[0].price_usd,
+          isLoading: false
+        });
+      })
+      .catch(error => {
+        console.error(error);
+      });
   }
 
   render() {
-    return (
-      <View>
- 
-          <TouchableOpacity 
-          onPress={this._onPressBtn}
-          style={styles.button1}
-          >  
-              <Text>LTC</Text>
-          
-          </TouchableOpacity>
-      </View>
-
-
-    );
+    if (this.state.isLoading) {
+      return (
+        <TouchableOpacity onPress={this._onPressBtn} style={styles.button1}>
+          <Text>LTC</Text>
+        </TouchableOpacity>
+      );
+    } else {
+      //if I make this a touchable opacity then onClick I can set isLoading = false and hide the ticker
+      return <Text style={styles.result}>{this.state.dataSource}</Text>;
+    }
   }
-
 }
-
-
 
 const styles = StyleSheet.create({
   button1: {
-    height: 50,   
-    alignItems: 'center',
-    color: 'blue'
+    height: 50,
+    alignSelf: "center",
+    color: "blue"
+  },
+  result: {
+    alignSelf: "center",
+    height: 50
   },
   buttonText: {
     padding: 20,
-    color: 'white'
+    color: "white"
   }
 });
-
-
