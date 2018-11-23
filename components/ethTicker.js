55ead3059cd4d973e2a162c2cf1d4f907e15fc20
jwbnw <james.w.belford@gmail.com>
2018-11-22 19:39:42 -0800

    Updated eth fetch

--- components/ethTicker.js
+++ components/ethTicker.js
@@ -1,51 +1,65 @@
-import React, { Component } from 'react';
-import { Alert, AppRegistry, StyleSheet, View, TouchableOpacity, Text } from 'react-native';
+import React, { Component } from "react";
+import {
+  Alert,
+  AppRegistry,
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  Text
+} from "react-native";
 
-
-  export default class EthTicker extends React.Component {
-
-	constructor(props){
+export default class EthTicker extends React.Component {
+  constructor(props) {
     super(props);
-    this.state = { isLoading: true}
-  }
+    this.state = { isLoading: true };
 
+    this._onPressBtn = this._onPressBtn.bind(this);
+    this.fetchData = this.fetchData.bind(this);
+  }
 
   _onPressBtn() {
-    Alert.alert('ETH!')
+    this.fetchData();
   }
 
+  fetchData() {
+    fetch("https://api.coinmarketcap.com/v1/ticker/ethereum/")
+      .then(response => response.json())
+      .then(responseJSON => {
+        this.setState({
+          dataSource: responseJSON[0].price_usd,
+          isLoading: false
+        });
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
 
   render() {
-    return (
-      <View>
- 
-          <TouchableOpacity 
-          onPress={this._onPressBtn}
-          style={styles.button1}
-          >  
-              <Text>ETH</Text>
-          
-          </TouchableOpacity>
-      </View>
-
-
-    );
+    if (this.state.isLoading) {
+      return (
+        <TouchableOpacity onPress={this._onPressBtn} style={styles.button1}>
+          <Text>ETH</Text>
+        </TouchableOpacity>
+      );
+    } else {
+      return <Text style={styles.result}>{this.state.dataSource}</Text>;
+    }
   }
-
 }
-
-
 
 const styles = StyleSheet.create({
   button1: {
-    height: 50,   
-    alignItems: 'center',
-    color: 'blue'
+    height: 50,
+    alignSelf: "center",
+    color: "blue"
+  },
+  result: {
+    alignSelf: "center",
+    height: 50
   },
   buttonText: {
     padding: 20,
-    color: 'white'
+    color: "white"
   }
 });
-
-
