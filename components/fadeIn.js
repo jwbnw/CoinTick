83844a850b281d879a473b36d2d7f01b092486a8
jwbnw <james.w.belford@gmail.com>
2018-11-24 20:15:51 -0800

    Adjusted fade time

--- components/fadeIn.js
+++ components/fadeIn.js
@@ -3,28 +3,30 @@ import { Animated, Text, View } from "react-native";
 
 export default class FadeInView extends React.Component {
   state = {
+
     fadeAim: new Animated.Value(0) // opacity:0
   };
 
   componentDidMount() {
     Animated.timing(this.state.fadeAim, {
       toValue: 1,
-      duration: 8000
+      duration: 7000
     }).start();
   }
 
   render() {
     let { fadeAim } = this.state;
 
-    return (
+    return (       
       <Animated.View
         style={{
           ...this.props.style,
           opacity: fadeAim
-        }}
+        }} 
       >
         {this.props.children}
       </Animated.View>
     );
+
   }
 }
