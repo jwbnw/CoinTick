93530ef37d0085591bf91b71805e502596528e1d
jwbnw <james.w.belford@gmail.com>
2018-12-09 10:52:14 -0800

    formatting

--- __tests__/btcTicker-test.js
+++ __tests__/btcTicker-test.js
@@ -2,34 +2,34 @@ import React from "react";
 import BtcTicker from "./../components/btcTicker";
 
 import renderer from "react-test-renderer";
-import {shallow} from 'enzyme';
+import { shallow } from "enzyme";
 
 test("BTC Ticker Renders Correctly", () => {
   const tree = renderer.create(<BtcTicker />).toJSON();
   expect(tree).toMatchSnapshot();
 });
 
-describe('BtcTicker component', () => {
-  it('Constructor should set isLoading to true', () => {
-    const wrapper = shallow(<BtcTicker/>);
+describe("BtcTicker component", () => {
+  it("Constructor should set isLoading to true", () => {
+    const wrapper = shallow(<BtcTicker />);
     expect(wrapper.instance().state.isLoading).toBe(true);
   });
-  it('Constructor should set dataSource to an empty string', () => {
-    const wrapper = shallow(<BtcTicker/>);
+  it("Constructor should set dataSource to an empty string", () => {
+    const wrapper = shallow(<BtcTicker />);
     expect(wrapper.instance().state.dataSource).toBe("");
   });
-  it('_onPresBtn1 should set isLoading to true', () => {
-    const wrapper = shallow(<BtcTicker/>);
+  it("_onPresBtn1 should set isLoading to true", () => {
+    const wrapper = shallow(<BtcTicker />);
     const instance = wrapper.instance();
     instance.state.isLoading = false;
     expect(instance.state.isLoading).toBe(false);
     instance._onPressBtn1();
     expect(instance.state.isLoading).toBe(true);
   });
-  it('_onPressBtn should call fetchData()', () => {
+  it("_onPressBtn should call fetchData()", () => {
     const fetchDataMock = jest.fn();
 
-    const wrapper = shallow(<BtcTicker/>);
+    const wrapper = shallow(<BtcTicker />);
     const instance = wrapper.instance();
     instance.fetchData = fetchDataMock;
     instance._onPressBtn();
@@ -38,4 +38,3 @@ describe('BtcTicker component', () => {
   // last test that would be nice would be mocking the api call to coinmarketcap and testing our
   // fetchData functionality.
 });
-
