6eef95939e345e549787f2c07e57bb2a14a833dd
jwbnw <james.w.belford@gmail.com>
2019-01-05 23:23:44 -0800

    fixing class name

--- screens/settings.js
+++ screens/settings.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { StyleSheet, View, Text } from "react-native";
 
-export default class BtcTicker extends React.Component {
+export default class Settings extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
