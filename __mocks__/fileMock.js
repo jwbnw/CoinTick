d759c62e81bf4a0f5d05de53c8533af2428ace5e
jwbnw <james.w.belford@gmail.com>
2019-01-05 19:54:10 -0800

    updating some snapshots

--- __mocks__/fileMock.js
+++ __mocks__/fileMock.js
@@ -1,3 +1,5 @@
+global.fetch = require('jest-fetch-mock')
+
 jest.mock('./assets/btc.png', ()=>'btc.png');
 jest.mock('./assets/ltc.png', ()=>'ltc.png');
 jest.mock('./assets/etc.png', ()=>'etc.png');
